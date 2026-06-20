ccd787cbb2fd5353585720dffb5cc8849fb372f1
petracb9 <pcbustillo9@gmail.com>
2026-06-20 10:29:09 -0300

    Modificado en la carpeta pages el archivo Carrito.jsx

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,4 +1,10 @@
-function Carrito({carrito}) {
+import {useCart} from '../context/CartContext'
+import { Link } from 'react-router-dom'
+import './Carrito.css'
+
+function Carrito() {
+  const { carrito, removeFromCart, clearCart, totalItems, totalPrecio } = useCart()
+
   return (
     <div className="carrito-container">
       <h2 className="page-title">Tu Carrito</h2>

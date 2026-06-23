358070c22596e7a2a070967dfe923cd0d4479a5f
petracb9 <pcbustillo9@gmail.com>
2026-06-22 23:42:27 -0300

    Modificado código App.jsx se agrega los elements Login,Registro,Perfil

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import Categorias from './pages/Categorias'
 import Productos from './pages/Productos'
 import ProductoDetalle from './pages/ProductoDetalle'
 import Carrito from './pages/Carrito'
+import Login from './pages/Login'
+import Registro from './pages/Registro'
+import Perfil from './pages/Perfil'
+import PrivateRoute from './components/auth/PrivateRoute'
 import './App.css'
 
 function App() {
@@ -16,7 +20,14 @@ function App() {
           <Route path="/productos" element={<Productos />} />
           <Route path="/producto/:id" element={<ProductoDetalle />} />
           <Route path="/carrito" element={<Carrito />} />
-         </Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/registro" element={<Registro />} />
+          <Route path="/perfil" element={
+            <PrivateRoute>
+              <Perfil />
+            </PrivateRoute>
+          } />
+        </Routes>
       </Layout>  
   )
 }

45ba13556561f3efd9812e74a83629ab7a8481e9
petracb9 <pcbustillo9@gmail.com>
2026-06-23 13:07:57 -0300

    Modificado código Navbar.css, se genera propiedades Login, Registro, Perfil

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom'
 import { useCart } from '../../context/CartContext'
+import { useAuth } from '../../context/AuthContext'
 import './Navbar.css'
 
 function Navbar() {
   const { totalItems } = useCart()
+  const { user, logout } = useAuth()
 
   return (
     <nav className="navbar">
@@ -17,6 +19,18 @@ function Navbar() {
           {totalItems > 0 && (<span className='cart-count'>{totalItems}</span>)}
           </Link>
         </li>
+        {!user ? (
+          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/registro">Registro</Link></li>
+          
+          </>                 
+        ) : (
+          <>
+            <li><Link to="/perfil">Perfil</Link></li>
+            <li><button onClick={logout} className='nav-logout'>Cerrar Sesión</button></li>
+          </>
+        )}
       </ul>
     </nav>
   )

97a65a9353aedd1e3986130b13a0bb26e1590eea
petracb9 <pcbustillo9@gmail.com>
2026-07-04 18:42:31 -0300

    Modificado códigos eliminada líneas en archivo Layout.css y Layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,4 +1,3 @@
-/*import Header from './Header'*/
 import Footer from './Footer'
 import './Layout.css'
 
@@ -6,7 +5,6 @@ import './Layout.css'
 function Layout ({ children }) {
   return (
     <div className="layout">
-      {/*<Header />eliminar aqui lo comentado si funciona la App*/ }
       <main className="main-content">
         {children}
       </main>
